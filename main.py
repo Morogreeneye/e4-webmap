41e08f364879b05bbfde8ae0c4bb46cf1fa87870
Morogreeneye <jillianchatelain@gmail.com>
2021-11-08 15:32:34 -0500

    Mon 11/8

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import folium
+import csv
+
 MAPNAME = "data/map1.html"
 
-if __name__=="__main__":
+if __name__ == "__main__":
     print("Hello gang.")
     map = folium.Map(location=(35.61, -82.44))
     map.save('map1.html')
+
+    with open("volcano-events.txt") as csvfile:
+        sniffsnaff = csv.Sniffer().sniff(csvfile.read())
+        csvfile.seek(0)
+        reader = csv.DictReader(csvfile, dialect=sniffsnaff)
+        rows = []
+        for row in reader:
+            rows.append(row)
